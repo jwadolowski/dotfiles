940e0a422e985dd9f4096b32a3939818782345ab
jwadolowski <6334715+jwadolowski@users.noreply.github.com>
2019-07-23 11:27:07 +0200

    Installation of brew, oh-my-zsh and oh-my-zsh plugins

--- install.sh
+++ install.sh
@@ -15,6 +15,61 @@ declare -r CONFIG_ROOT_DIR="${HOME}/.config"
 declare -r OH_MY_ZSH_ROOT_DIR="${HOME}/.oh-my-zsh"
 
 # -----------------------------------------------------------------------------
+# Packages/plugins
+# -----------------------------------------------------------------------------
+OH_MY_ZSH_PLUGINS=(
+    https://github.com/zsh-users/zsh-completions
+    https://github.com/zsh-users/zsh-syntax-highlighting
+)
+
+BREW_PACKAGES=(
+    alexjs
+    bat
+    coreutils
+    ctags
+    diff-so-fancy
+    fd
+    fzf
+    git
+    glances
+    hadolint
+    htop
+    jq
+    jsonlint
+    ncdu
+    neovim
+    prettier
+    prettyping
+    pstree
+    python
+    python2
+    ripgrep
+    shellcheck
+    shfmt
+    terraform@0.11
+    tflint
+    tldr
+    tree
+    vale
+    watch
+    wget
+    yamllint
+    zsh
+)
+
+BREW_CASK_PACKAGES=(
+    adoptopenjdk8
+    firefox
+    font-hack-nerd-font
+    google-chrome
+    iterm2
+    spectacle
+    sourcetree
+)
+
+GEM_PACKAGEs=()
+
+# -----------------------------------------------------------------------------
 # Colors
 # -----------------------------------------------------------------------------
 declare -r RED_COLOR='\033[0;31m'
@@ -40,6 +95,51 @@ function error_log() {
 function output_separator() {
     echo -e "---"
 }
+
+function install_oh_my_zsh_plugin() {
+    plugin_name=$(basename "${1}")
+    plugin_dir="${OH_MY_ZSH_ROOT_DIR}/custom/plugins/${plugin_name}"
+
+    if [[ ! -d ${plugin_dir} ]]; then
+        info_log "Installing ${plugin_name}..."
+        # git clone ${1} ${}
+    else
+        info_log "${plugin_name} is already installed"
+    fi
+}
+
+# -----------------------------------------------------------------------------
+# brew.sh
+# -----------------------------------------------------------------------------
+if [[ $(command -v brew) == "" ]]; then
+    info_log "Intalling brew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    info_log "brew is already installed"
+fi
+
+output_separator
+
+# -----------------------------------------------------------------------------
+# oh-my-zsh
+# -----------------------------------------------------------------------------
+if [[ ! -d $OH_MY_ZSH_ROOT_DIR ]]; then
+    info_log "Installing oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    info_log "oh-my-zsh is already installed"
+fi
+
+output_separator
+
+# -----------------------------------------------------------------------------
+# oh-my-zsh plugins
+# -----------------------------------------------------------------------------
+for p in "${OH_MY_ZSH_PLUGINS[@]}"; do
+    install_oh_my_zsh_plugin "$p"
+done
+
+output_separator
 
 # -----------------------------------------------------------------------------
 # Deploy ~/.config
