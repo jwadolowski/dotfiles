7d464bf5b68355ccaa3467475f69e395a12bfa94
jwadolowski <6334715+jwadolowski@users.noreply.github.com>
2023-02-26 15:44:19 +0100

    fix: Always use 'brew --prefix' when referencing brew root dir

--- oh-my-zsh/custom/path.zsh
+++ oh-my-zsh/custom/path.zsh
@@ -1,17 +1,19 @@
+BREW_PREFIX=$(brew --prefix)
+
 # Prefer coreutils binaries over built-in macOS ones
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+export PATH="${BREW_PREFIX}/opt/coreutils/libexec/gnubin:${PATH}"
 
 # Add gnu-sed to the PATH
-export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:${PATH}"
+export PATH="${BREW_PREFIX}/opt/gnu-sed/libexec/gnubin:${PATH}"
 
 # Add GNU grep to the PATH
-export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+export PATH="${BREW_PREFIX}/opt/grep/libexec/gnubin:$PATH"
 
 # Prefer curl installed via brew
-export PATH="/usr/local/opt/curl/bin:${PATH}"
+export PATH="${BREW_PREFIX}/opt/curl/bin:${PATH}"
 
 # https://github.com/Homebrew/homebrew-core/issues/14669#issuecomment-353399229
-export PATH="/usr/local/sbin:${PATH}"
+export PATH="${BREW_PREFIX}/sbin:${PATH}"
 
 # Use node.js 16 by default
-export PATH="/usr/local/opt/node@16/bin:$PATH"
+export PATH="${BREW_PREFIX}/opt/node@16/bin:$PATH"
