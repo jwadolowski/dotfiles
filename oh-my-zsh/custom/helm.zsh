066260a80352db996a1da25671d69aff1d47dd03
jwadolowski <6334715+jwadolowski@users.noreply.github.com>
2024-02-21 23:21:36 +0100

    fix: Minor htb func changes (display both stderr and stdout on 'helm template' error)

--- oh-my-zsh/custom/helm.zsh
+++ oh-my-zsh/custom/helm.zsh
@@ -6,8 +6,17 @@ function htv() {
 }
 
 function htb() {
-  local out_file=$(mktemp --suffix ".yaml")
-  make template 2>/dev/null | tail -n +6 > $out_file
-  bat $out_file
-  rm -f $out_file
+  local stdout_file=$(mktemp --suffix ".yaml")
+  local stderr_file=$(mktemp --suffix ".txt")
+
+  make template 2>"${stderr_file}" > "${stdout_file}"
+
+  if [[ $? == 0 ]]; then
+    tail -n +6 "${stdout_file}" | bat --language yaml
+  else
+    bat "${stderr_file}"
+    tail -n +6 "${stdout_file}" | bat --language yaml
+  fi
+
+  rm -f "${stdout_file}" "${stderr_file}"
 }
