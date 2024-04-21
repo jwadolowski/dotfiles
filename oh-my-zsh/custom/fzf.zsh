762c53ecddf6dcf032632a30cf189074f6207c16
jwadolowski <6334715+jwadolowski@users.noreply.github.com>
2024-04-21 18:29:39 +0200

    feat(fzf): New tokyonight-night fzf colors

--- oh-my-zsh/custom/fzf.zsh
+++ oh-my-zsh/custom/fzf.zsh
@@ -5,9 +5,11 @@ export FZF_CTRL_T_OPTS="--preview-window 'right:60%' --preview 'bat --color=alwa
 
 # ----------------
 # tokyonight-night
+#
+# Ref: https://github.com/folke/tokyonight.nvim/blob/main/extras/fzf/tokyonight_night.zsh
 # ----------------
 export FZF_DEFAULT_OPTS="--height 40% --no-multi --select-1 --exit-0 \
-  --color=fg:#c0caf5,bg:#1a1b26,hl:#bb9af7 \
-	--color=fg+:#c0caf5,bg+:#1a1b26,hl+:#7dcfff \
-	--color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff \
-	--color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a"
+  --color=fg:#c0caf5,bg:#1a1b26,hl:#ff9e64 \
+  --color=fg+:#c0caf5,bg+:#292e42,hl+:#ff9e64 \
+  --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff \
+  --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a"
