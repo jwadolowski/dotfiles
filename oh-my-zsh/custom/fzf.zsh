16a67dc2ea24d417121618d5d66d96f4b909480f
jwadolowski <6334715+jwadolowski@users.noreply.github.com>
2024-04-22 18:26:31 +0200

    feat(fzf): --no-ignore-vcs flag removed from fd command

--- oh-my-zsh/custom/fzf.zsh
+++ oh-my-zsh/custom/fzf.zsh
@@ -1,5 +1,5 @@
 # .ignore is taken into account while execuiting fd/rg/ag
-export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --no-ignore-vcs'
+export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
 
