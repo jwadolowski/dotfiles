5f3bfd6e92852768d22a85e3597a9c88f119429c
jwadolowski <6334715+jwadolowski@users.noreply.github.com>
2024-07-16 20:20:36 +0200

    chore: fzf color update

--- oh-my-zsh/custom/fzf.zsh
+++ oh-my-zsh/custom/fzf.zsh
@@ -2,14 +2,35 @@
 export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
+export FZF_DEFAULT_OPTS="--height 40% --no-multi --select-1 --exit-0"
 
 # ----------------
 # tokyonight-night
 #
-# Ref: https://github.com/folke/tokyonight.nvim/blob/main/extras/fzf/tokyonight_night.zsh
+# Ref: https://github.com/folke/tokyonight.nvim/blob/main/extras/fzf/tokyonight_night.sh
+#
+# Background color changed to #1a1b26 to match iterm color
 # ----------------
-export FZF_DEFAULT_OPTS="--height 40% --no-multi --select-1 --exit-0 \
-  --color=fg:#c0caf5,bg:#1a1b26,hl:#ff9e64 \
-  --color=fg+:#c0caf5,bg+:#292e42,hl+:#ff9e64 \
-  --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff \
-  --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a"
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none
+  --color=bg+:#283457 \
+  --color=bg:#1a1b26 \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c \
+"
