cb18b6719ba92d42317897b668d410f69753e019
Jakub Wądołowski <6334715+jwadolowski@users.noreply.github.com>
2025-02-09 13:35:02 +0100

    feat(init): Redesign dotfiles initialization
    Deployment from scratch (brand new Mac) requires a few additional steps
    to make `init.sh` execution seamless.

--- init.sh
+++ init.sh
@@ -11,8 +11,41 @@ set -o nounset
 if [[ $(command -v brew) == "" ]]; then
   echo "Intalling brew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # At this stage brew is installed, but not yet initialized therefore not
+  # executable in scope of the current shell. Let's temporarily set it up
+  # so that 'brew' comand can be called.
+  #
+  # Brew configuration is persisted in zimfw's config file that gets deployed
+  # in subseqnet steps.
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+
+  # Install core packages (referenced by Taskfile.yaml)
+  brew install \
+    zsh \
+    git \
+    starship \
+    eza \
+    stow \
+    go-task \
+    bat \
+    mise \
+    tflint \
+    vale
 else
   echo "brew is already installed"
 fi
 
+# Configure core packages
+task zimfw \
+  restow \
+  bat \
+  mise \
+  vale \
+  tflint
+
+# Some apps require Rosetta to be installed
+sudo softwareupdate --install-rosetta --agree-to-license
+
+# Install remaining packages
 brew bundle install
